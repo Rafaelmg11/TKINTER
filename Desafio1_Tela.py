d9711d98ffcc603f7c5c0086ed20bc754d77c8ac
Rafaelmg11 <almeidarafaelmagalhaes@gmail.com>
2025-02-25 19:57:06 -0300

    up

--- Desafio1_Tela.py
+++ Desafio1_Tela.py
@@ -4,19 +4,24 @@ i = Tk()
 i.title("Programa Financeiro")
 i.geometry('980x720+250+30')
 
-lb1 = Label(i,text="Senha",bg="yellow")
-lb1.grid(row=1,column=1)
+lb1 = Label(i,text="Login",bg="yellow")
+lb1.place(x=695,y=170)
 
 lb2=Label(i,text="Senha",bg="red")
-lb2.grid(row=6,column=6)
+lb2.place(x=695,y=250)
 
 ed1 = Entry(i)
-ed1.grid(row=3,column=3)
+ed1.place(x=650,y=150)
+
 
 ed2 = Entry(i)
-ed2.grid(row=4,column=4)
+ed2.place(x=650,y=230)
+
 
 bt1=Button(i,text="Login")
-bt1.grid(row=5,column=5)
+bt1.place(x=455,y=500)
+
+
 
 i.mainloop()
+
---
+++ Tkinter_Coluna_Linha.py
@@ -0,0 +1,41 @@
+from tkinter import *
+
+i = Tk()
+i.title("Programa Financeiro")
+i.geometry('980x720+250+30')
+
+# lb1 = Label(i,text="Login",bg="yellow")
+# lb1.grid(row=2,column=2)
+
+# lb2=Label(i,text="Senha",bg="red")
+# lb2.grid(row=2,column=2)
+
+# ed1 = Entry(i)
+# ed1.grid(row=2,column=2)
+
+# ed2 = Entry(i) 
+# ed2.grid(row=2,column=2)
+
+# bt1=Button(i,text="Login")
+# bt1.grid(row=2,column=2)
+
+#O codigo abaixo faz correção das posições das labels
+lb1 = Label(i,text="Login",bg="yellow")
+lb1.grid(row=1,column=1)
+
+lb2=Label(i,text="Senha",bg="red") 
+lb2.grid(row=2,column=1)
+
+ed1 = Entry(i)
+ed1.grid(row=1,column=2)
+
+ed2 = Entry(i) 
+ed2.grid(row=2,column=2)
+
+bt1=Button(i,text="Login")
+bt1.grid(row=3,column=1)
+
+
+
+i.mainloop()
+
